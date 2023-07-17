aa89d6abf4dd22c17fc531717be7f9c37c55187c
m-affum <maffum73@gmail.com>
2023-07-17 09:52:18 -0700

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - runs the main code
+*Return: returns a success value
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	printf("\n")
+	return (0);
+}
