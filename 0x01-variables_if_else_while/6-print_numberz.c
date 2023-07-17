a98cc4eb7a10afc36514b5e4ece4789f60d7e006
m-affum <maffum73@gmail.com>
2023-07-17 10:09:46 -0700

    added task 6 and 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - runs the main code
+*Return: returns a success value
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - runs the main code
+*Return: returns a success value
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 122; c >= 97; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
