d69d9d1c3618da050f73ebfcc05ffd3784ff9fa8
m-affum <maffum73@gmail.com>
2023-07-17 11:51:37 -0700

    added task 1 to 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - runs the main code
+* Return: returns the value of success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
