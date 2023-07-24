d5f015dad822b53642c2bc1d77d5fac18f64d205
m-affum <maffum73@gmail.com>
2023-07-24 08:14:00 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * Prints putchar to the standard output'
+ * Main -entry point
+ * Returns (0),void 
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning Nested loops in c
