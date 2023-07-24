3b114d2521d3e78679e387a2c9621c0fa547b1ef
m-affum <maffum73@gmail.com>
2023-07-24 09:45:32 -0700

    Prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * prints aplhabets in lower case
+ * return 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
